6760711557b406e980b87a00634b17820c4b85cf
zhaoshuxue <zhaoshuxue@163.com>
2020-03-14 21:55:34 +0800

    commit 2020-03-14 21:55:33

---
+++ markdown/src/test/java/com/zsx/md/MarkdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsx.md;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
