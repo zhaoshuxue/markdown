e9f9b4ba5d9490a513ad468b4671c7f37c0182b1
zhaoshuxue <zhaoshuxue@163.com>
2020-03-19 23:26:04 +0800

    commit 2020-03-19 23:26:03

---
+++ markdown/src/main/java/com/zsx/md/entity/Mnote.java
@@ -0,0 +1,16 @@
+package com.zsx.md.entity;
+
+import lombok.Data;
+
+@Data
+public class Mnote {
+
+    private Integer id;
+
+    private Integer pid;
+
+    private String title;
+
+    private String summary;
+
+}
