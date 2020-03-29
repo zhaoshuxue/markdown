ed52aa5a587e8e0ddf3edcd53e77dde2cb16b4d1
zhaoshuxue <zhaoshuxue@163.com>
2020-03-29 21:41:05 +0800

    commit 2020-03-29 21:41:04

--- markdown/src/main/java/com/zsx/md/service/impl/NoteServiceImpl.java
+++ markdown/src/main/java/com/zsx/md/service/impl/NoteServiceImpl.java
@@ -3,7 +3,6 @@ package com.zsx.md.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.sun.xml.internal.bind.v2.TODO;
 import com.zsx.md.config.PropertiesConfig;
 import com.zsx.md.entity.Mnote;
 import com.zsx.md.service.NoteService;
