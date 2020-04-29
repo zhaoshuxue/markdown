d6d1a40f62cce2181c37b44109735a3df56d1562
zhaoshuxue <zhaoshuxue@163.com>
2020-04-29 23:35:00 +0800

    add

--- markdown/src/main/java/com/zsx/md/controller/IndexController.java
+++ markdown/src/main/java/com/zsx/md/controller/IndexController.java
@@ -4,12 +4,15 @@ import com.baomidou.kisso.SSOHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.UUID;
 
 @Controller
 public class IndexController {
-
 
 
     @GetMapping("/index")
@@ -21,9 +24,12 @@ public class IndexController {
     @GetMapping("/")
     public String index2(Model model) {
         model.addAttribute("name", UUID.randomUUID().toString());
+        return "index";
+    }
 
-
-
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        SSOHelper.clearRedirectLogin(request, response);
         return "index";
     }
 
